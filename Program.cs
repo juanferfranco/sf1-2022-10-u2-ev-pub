0abefb707a9b8eecd157823503951ef84de0c9cf
juanUPBLap <juanf.franco@upb.edu.co>
2022-03-17 07:51:41 -0500

    init repo

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace snakeSensores
+{
+    enum Direction
+    {
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3,
+    }
+
+    enum Tile
+    {
+        Open = 0,
+        Snake,
+        Food,
+    }
+
+    class Program
+    {
+        static char[] DirectionChars = { '^', 'v', '<', '>', };
+        static TimeSpan sleep = TimeSpan.FromMilliseconds(100);
+        static int width = Console.WindowWidth;
+        static int height = Console.WindowHeight;
+        static Random random = new Random();
+        static Tile[,] map = new Tile[width, height];
+        static Direction? direction = null;
+        static Queue<(int X, int Y)> snake = new Queue<(int X, int Y)>();
+        static (int X, int Y) position;
+        static bool closeRequested = false;
+
+        static void Main(string[] args)
+        {
+
+            position.X = width / 2;
+            position.Y = height / 2;
+            try
+            {
+                Console.CursorVisible = false;
+                Console.Clear();
+                snake.Enqueue(position);
+                map[position.X, position.Y] = Tile.Snake;
+                PositionFood();
+                Console.SetCursorPosition(position.X, position.Y);
+                Console.Write('@');
+
+                Thread serialThr = new Thread(serialComm);
+                serialThr.Start();
+                Thread keyboardThr = new Thread(keyboard);
+                keyboardThr.Start();
+
+
+                while (!direction.HasValue && !closeRequested) ;
+
+                while (!closeRequested)
+                {
+                    if (Console.WindowWidth != width || Console.WindowHeight != height)
+                    {
+                        Console.Clear();
+                        Console.Write("Console was resized. Snake game has ended.");
+                        return;
+                    }
+                    switch (direction)
+                    {
+                        case Direction.Up: position.Y--; break;
+                        case Direction.Down: position.Y++; break;
+                        case Direction.Left: position.X--; break;
+                        case Direction.Right: position.X++; break;
+                    }
+                    if (position.X < 0 || position.X >= width ||
+                        position.Y < 0 || position.Y >= height ||
+                        map[position.X, position.Y] is Tile.Snake)
+                    {
+                        Console.Clear();
+                        Console.Write("Game Over. Score: " + (snake.Count - 1) + ".");
+                        return;
+                    }
+                    Console.SetCursorPosition(position.X, position.Y);
+                    Console.Write(DirectionChars[(int)direction]);
+                    snake.Enqueue(position);
+                    if (map[position.X, position.Y] == Tile.Food)
+                    {
+                        PositionFood();
+                    }
+                    else
+                    {
+                        (int x, int y) = snake.Dequeue();
+                        map[x, y] = Tile.Open;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(' ');
+                    }
+                    map[position.X, position.Y] = Tile.Snake;
+
+                    Thread.Sleep(sleep);
+                }
+                Console.Clear();
+            }
+            finally
+            {
+                Console.CursorVisible = true;
+                Environment.Exit(0);
+
+            }
+        }
+
+        static void GetDirection()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow: direction = Direction.Up; break;
+                case ConsoleKey.DownArrow: direction = Direction.Down; break;
+                case ConsoleKey.LeftArrow: direction = Direction.Left; break;
+                case ConsoleKey.RightArrow: direction = Direction.Right; break;
+                case ConsoleKey.Escape: closeRequested = true; break;
+            }
+        }
+
+        static void PositionFood()
+        {
+            int xvalue;
+            int yvalue;
+
+            while (true)
+            {
+                xvalue = random.Next(width);
+                yvalue = random.Next(height);
+                if (map[xvalue, yvalue] is Tile.Open) break;
+            }
+            map[xvalue, yvalue] = Tile.Food;
+            Console.SetCursorPosition(xvalue, yvalue);
+            Console.Write('+');
+        }
+
+        static void keyboard()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    GetDirection();
+                }
+
+            }
+
+        }
+
+        static void serialComm()
+        {
+            // 1. Inicializa y abre el puerto serial
+            while (true)
+            {
+                // 2. Implementa la lógica con tu solución.
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ sensores/sensores.ino
@@ -0,0 +1,183 @@
+#define DEBUG
+
+#ifdef DEBUG
+#include "SSD1306Wire.h"
+static SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
+#define LOG(msg) display.clear();display.drawString(display.width()/2, display.height()/2, msg); display.display()
+#define INITLOG() display.init(); display.setContrast(255); display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH); display.setFont(ArialMT_Plain_16); display.clear()
+#else
+#define LOG(msg)
+#define INITLOG()
+#endif
+
+#define LED1 25
+#define UP_BTN 13
+#define DOWN_BTN 12
+#define LEFT_BTN 32
+#define RIGHT_BTN 33
+
+
+static const char *buttonGetName(uint8_t number) {
+  static const char *buttonName[] = {"NONE", "UP", "DOWN", "LEFT", "RIGHT"};
+  uint8_t index = 0;
+  switch (number) {
+    case UP_BTN:
+      index = 1;
+      break;
+    case DOWN_BTN:
+      index = 2;
+      break;
+    case LEFT_BTN:
+      index = 3;
+      break;
+    case RIGHT_BTN:
+      index = 4;
+      break;
+    default:
+      break;
+  }
+  return buttonName[index];
+}
+
+void taskButtons();
+void taskSerial();
+void taskBeat();
+
+void setup() {
+  INITLOG();
+  taskButtons();
+  taskSerial();
+  taskBeat();
+}
+
+
+// EVENTOS
+bool evButtons = false;
+uint8_t evButtonsData = 0;
+
+void loop() {
+  taskButtons();
+  taskSerial();
+  taskBeat();
+}
+
+
+
+void taskButtons() {
+  enum class ButtonsStates {INIT, WAITING_PRESS, WAITING_STABLE, WAITING_RELEASE};
+  static ButtonsStates buttonsState =  ButtonsStates::INIT;
+  static uint8_t lastButton = 0;
+  static uint32_t referenceTime;
+  const uint32_t STABLETIMEOUT = 50;
+
+  switch (buttonsState) {
+    case ButtonsStates::INIT: {
+        pinMode(UP_BTN, INPUT_PULLUP);
+        pinMode(DOWN_BTN, INPUT_PULLUP);
+        pinMode(LEFT_BTN, INPUT_PULLUP);
+        pinMode(RIGHT_BTN, INPUT_PULLUP);
+        buttonsState = ButtonsStates::WAITING_PRESS;
+        break;
+      }
+    case ButtonsStates::WAITING_PRESS: {
+        if (digitalRead(UP_BTN) == LOW) {
+          buttonsState = ButtonsStates::WAITING_STABLE;
+          lastButton = UP_BTN;
+          referenceTime = millis();
+        }
+        else if (digitalRead(DOWN_BTN) == LOW) {
+          buttonsState = ButtonsStates::WAITING_STABLE;
+          lastButton = DOWN_BTN;
+          referenceTime = millis();
+        }
+        else if (digitalRead(LEFT_BTN) == LOW) {
+          buttonsState = ButtonsStates::WAITING_STABLE;
+          lastButton = LEFT_BTN;
+          referenceTime = millis();
+        }
+        else if (digitalRead(RIGHT_BTN) == LOW) {
+          buttonsState = ButtonsStates::WAITING_STABLE;
+          lastButton = RIGHT_BTN;
+          referenceTime = millis();
+        }
+        break;
+      }
+
+    case ButtonsStates::WAITING_STABLE: {
+        if (digitalRead(lastButton) == HIGH) {
+          buttonsState = ButtonsStates::WAITING_PRESS;
+        }
+        else if ( (millis() - referenceTime) >= STABLETIMEOUT ) {
+          buttonsState = ButtonsStates::WAITING_RELEASE;
+        }
+        break;
+      }
+
+    case ButtonsStates::WAITING_RELEASE: {
+        if (digitalRead(lastButton) == HIGH) {
+          buttonsState = ButtonsStates::WAITING_PRESS;
+          evButtons = true;
+          evButtonsData = lastButton;
+          LOG(String( String(buttonGetName(lastButton)) ));
+        }
+
+        break;
+      }
+    default:
+      break;
+
+  }
+}
+
+void taskSerial() {
+  enum class SerialStates {INIT, WAITING_REQUESTS};
+  static SerialStates serialState =  SerialStates::INIT;
+
+  switch (serialState) {
+    case SerialStates::INIT: {
+
+
+        break;
+      }
+    case SerialStates::WAITING_REQUESTS: {
+
+
+        break;
+      }
+    default:
+      break;
+  }
+}
+
+void taskBeat() {
+  enum class BeatStates {INIT, BEATING};
+  static BeatStates beatlState =  BeatStates::INIT;
+  static uint32_t previousMillis = 0;
+  const uint32_t interval = 500;
+  static bool ledState = false;
+
+  switch (beatlState) {
+    case BeatStates::INIT: {
+        digitalWrite(LED1, ledState);
+        pinMode(LED1, OUTPUT);
+        beatlState = BeatStates::BEATING;
+        break;
+      }
+    case BeatStates::BEATING: {
+        if ( (millis() - previousMillis) >= interval) {
+          previousMillis = millis();
+
+          if (ledState == false) {
+            ledState = true;
+          } else {
+            ledState = false;
+          }
+          digitalWrite(LED1, ledState);
+        }
+
+        break;
+      }
+    default:
+      break;
+  }
+}
